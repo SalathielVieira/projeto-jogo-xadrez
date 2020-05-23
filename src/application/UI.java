0acf4501af35f0b33aea1ea221449fb8762a358e
Salathiel Vieira <salathielvieira@outlook.com>
2020-05-23 00:00:52 -0400

    Classe Peão

--- src/application/UI.java
+++ src/application/UI.java
@@ -91,7 +91,7 @@ public class UI {
 
     private static void printPiece(ChessPiece piece, boolean background) {
         if (background) {
-            System.out.print(ANSI_YELLOW_BACKGROUND);
+            System.out.print(ANSI_PURPLE_BACKGROUND);
         }
         if (piece == null) {
             System.out.print("-" + ANSI_RESET);
